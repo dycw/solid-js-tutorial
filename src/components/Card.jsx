84615e2e0089db514cb2ea6a42b7b385701206a2
Derek Wan <d.wan@icloud.com>
2023-05-17 21:34:28 +0800

    Finish lesson 4 (global styles & tailwind)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card() {
   return (
-    <div>
+    <div class="bg-white p-4 text-center rounded-md shadow-ms">
       <h2>Card Component</h2>
       <p>
         Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
@@ -15,6 +15,7 @@ export default function Card() {
         Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
         et culpa duis.
       </p>
+      <button class="btn">Click me</button>
     </div>
   );
 }
