43865a7d61db9751729fd05f5c362ab03160d0ec
Derek Wan <d.wan@icloud.com>
2023-05-17 22:00:57 +0800

    Finish lesson 7 (event handlers) (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { createSignal } from "solid-js";
 import banner from "./assets/banner.png";
 import Card from "./components/Card";
 
 function App() {
+  const [darkTheme, setDarkTheme] = createSignal(false);
+
+  const toggleTheme = () => {
+    setDarkTheme((theme) => !theme);
+  };
+
   return (
     <div class="container m-auto">
-      <header>
+      <header
+        class="my-4 p-2 text-xl flex items-center gap-4"
+        classList={{ "bg-neutral-900": darkTheme(), "text-white": darkTheme() }}
+      >
+        <span
+          class="material-symbols-outlined cursor-pointer"
+          onclick={toggleTheme}
+        >
+          light_mode
+        </span>
         <h1>Ninja Merch</h1>
       </header>
       <img class="rounded-md" src={banner} alt="site banner" />
