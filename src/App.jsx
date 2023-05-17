0d205cb694762f872a78b617723da1de3dde3111
Derek Wan <d.wan@icloud.com>
2023-05-17 21:35:00 +0800

    Finish lesson 5 (props)

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,18 @@ function App() {
       </header>
       <img class="rounded-md" src={banner} alt="site banner" />
       <div class="grid grid-cols-4 gap-10 my-4">
-        <Card />
-        <Card />
-        <Card />
+        {/* <Card title='ninja tee' /> */}
+        <Card rounded={true} flat={false}>
+          <h2>Ninja Tee, Black</h2>
+          <p>Lorem ipsum!</p>
+          <button class="btn">view</button>
+        </Card>
+        <Card rounded={false} flat={true}>
+          <h2>Ninja Tee, White</h2>
+          <button class="btn">view</button>
+          <p>Lorem ipsum!</p>
+          <p>Only $10</p>
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,10 @@
-export default function Card() {
+export default function Card(props) {
   return (
-    <div class="bg-white p-4 text-center rounded-md shadow-ms">
-      <h2>Card Component</h2>
-      <p>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
-        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
-        Lorem duis laboris cupidatat officia voluptate. Culpa proident
-        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
-        et culpa duis.
-      </p>
-      <button class="btn">Click me</button>
+    <div
+      class="bg-white p-4 text-center"
+      classList={{ "rounded-md": props.rounded, "shadow-md": !props.flat }}
+    >
+      {props.children}
     </div>
   );
 }
